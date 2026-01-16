dbd0de04f0300199b71756355c5edccba2b7cdad
enir <52084661+enir101@users.noreply.github.com>
2026-01-15 20:16:41 -0500

    Update main.js

--- browser/src/main.js
+++ browser/src/main.js
@@ -16,12 +16,12 @@
 /* global ServerConnectionService createEmscriptenModule */
 /*eslint indent: [error, "tab", { "outerIIFEBody": 0 }]*/
 
-import { HazardFlag } from '@remora-llc/protocol/analysis-common';
+// import { HazardFlag } from '@remora-llc/protocol/analysis-common';
 
 (function (global) {
 
-// var { TelemetryClient, WebSocketTransport } = require('@remora-llc/telemetry');
-// var { HazardFlag } = require('@remora-llc/protocol/analysis-common')
+// eslint-disable-next-line no-undef
+var { HazardFlag } = require('@remora-llc/protocol/analysis-common');
 
 console.log('[INIT] bundle start');
 
